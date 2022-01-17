81ed6c9f410a391c629467c594eaff6f0aedc880
Débora Bucci <dbucci@protonmail.com>
2022-01-17 15:26:59 -0300

    create a new event listener when the <form> is submitted, and store all the values in the states inside an object

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -18,8 +18,18 @@ const ExpenseForm = () => {
     setDate(e.target.value);
   };
 
+  const submitHandler = (e) => {
+    e.preventDefalt();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
