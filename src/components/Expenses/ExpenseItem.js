b9d4b3cb9597e287fd15f4edf15acbbb71bcfeef
Débora Bucci <dbucci@protonmail.com>
2022-01-17 08:23:58 -0300

    add a button to change the title of the expense, and implement a turnery operator inside of the title <div> to retrieve an <h2> ir another <div> depending on if certain condition is correct. Then create functions to add the logic necessary to be able to change the title's content

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
-const ExpenseItem = ({ title, amount, date }) => {
+const ExpenseItem = ({ title: initialTitle, amount, date }) => {
+  const [title, setTitle] = useState(initialTitle);
+  const [change, setChange] = useState(false);
+
+  const changeHandler = () => {
+    setChange(true);
+  };
+
+  const inputHandler = (e) => {
+    const input = document.querySelector("input");
+    if (input.value.trim().length === 0) {
+      setChange(false);
+      return;
+    }
+
+    setTitle(input.value);
+    setChange(false);
+  };
+
+  console.log(title);
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        {change === false ? (
+          <h2>{title}</h2>
+        ) : (
+          <div>
+            <input placeholder="write here..."></input>
+            <button onClick={inputHandler}>Ok</button>
+          </div>
+        )}
         <div className="expense-item__price">{`$ ${amount}`}</div>
       </div>
+      <button onClick={changeHandler}>Change Title</button>
     </Card>
   );
 };
