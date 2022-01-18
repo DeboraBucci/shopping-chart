0abfc146e876683b21dbc8d488a00d92c82fa157
Débora Bucci <dbucci@protonmail.com>
2022-01-18 09:38:25 -0300

    implement filter functionality, and add a 'show all' option to the dropdown so all the years are shown when this option is selected

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,11 +11,21 @@ const Expenses = ({ expenses }) => {
     setFilteredYear(selectedYear);
   };
 
-  const expensesArr = [];
-  expenses.map(({ title, id, amount, date }) => {
+  const expensesPush = (title, id, amount, date) => {
     expensesArr.push(
       <ExpenseItem title={title} key={id} amount={amount} date={date} />
     );
+  };
+
+  const expensesArr = [];
+  expenses.map(({ title, id, amount, date }) => {
+    if (filteredYear === "show-all") {
+      expensesPush(title, id, amount, date);
+    }
+
+    if (date.getFullYear() === Number(filteredYear)) {
+      expensesPush(title, id, amount, date);
+    }
   });
   return (
     <Card className="expenses">
