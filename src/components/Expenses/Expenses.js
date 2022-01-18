367fba139819d0daae0799c54a7219d58ba74313
Débora Bucci <dbucci@protonmail.com>
2022-01-18 08:44:13 -0300

    create a new component called 'ExpensesFilter' and form a logic to display a dropdown with the years from 2019 to 2022, and store the picked year in a state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = ({ expenses }) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   const expensesArr = [];
   expenses.map(({ title, id, amount, date }) => {
     expensesArr.push(
       <ExpenseItem title={title} key={id} amount={amount} date={date} />
     );
   });
-  return <Card className="expenses">{expensesArr}</Card>;
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {expensesArr}
+    </Card>
+  );
 };
 
 export default Expenses;
